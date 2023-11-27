e1033e8bb28ff02d4fa50aea15e54b98fa7be266
DiKeySnakes <dikeysnakes@gmail.com>
2023-11-27 18:01:15 +0200

    Add components/icons.tsx, components/ui/form.tsx, components/ui/image-cropper.tsx, components/ui/input.tsx, components/ui/label.tsx, lib/prismadb.ts, prisma/schema.prisma, types/index.ts, modify package-lock.json, package.json

---
+++ types/index.ts
@@ -0,0 +1,50 @@
+import { Icons } from '@/components/icons';
+import { Comment, Image, Post, Prisma, User } from '@prisma/client';
+import { FileWithPath } from 'react-dropzone';
+
+export interface MainNav {
+  title: string;
+  href: string;
+  icon?: keyof typeof Icons;
+  iconFill?: keyof typeof Icons;
+  allowed?: boolean;
+  hidden?: boolean;
+  show?: boolean;
+  disabled?: boolean;
+}
+
+export interface SidebarNav {
+  title: string;
+  href: string;
+  icon?: keyof typeof Icons;
+  disabled?: boolean;
+}
+
+export type CommentWithUser = Prisma.CommentGetPayload<{
+  include: { author: true };
+}>;
+
+export type PostWithUser = Prisma.PostGetPayload<{
+  include: { user: true; comments: true };
+}>;
+
+export type extendedPost = Post & {
+  user: User;
+  comments: Comment[];
+  images: Image[];
+};
+
+export type NotificationWithUser = Prisma.NotificationGetPayload<{
+  include: { user: true };
+}>;
+
+export type FileWithPreview = FileWithPath & {
+  preview: string;
+};
+
+export enum FriendshipStatus {
+  FRIENDS = 'FRIENDS',
+  NOT_FRIENDS = 'NOT_FRIENDS',
+  REQUEST_SENT = 'REQUEST_SENT',
+  REQUEST_RECEIVED = 'REQUEST_RECEIVED',
+}
