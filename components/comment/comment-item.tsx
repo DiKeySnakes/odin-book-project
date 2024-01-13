85ffd323793583ec896a9b08035f312134631d89
DiKeySnakes <dikeysnakes@gmail.com>
2024-01-13 13:16:49 +0200

    Modify package.json, package-lock.json, add app/(auth)/sso-callback/page.tsx, components/auth/oauth-signin.tsx, components/auth/sso-callback.tsx, components/comment/comment-item.tsx, components/comment/comment-list.tsx, components/user-tooltip.tsx

---
+++ components/comment/comment-item.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { format } from 'date-fns';
+import * as React from 'react';
+import Link from 'next/link';
+import { formatDates } from '@/lib/utils';
+import { CommentWithUser } from '@/types';
+import { UserName } from '@/components/user/user-name';
+import { UserAvatar } from '@/components/user/user-avatar';
+import { UserTooltip } from '@/components/user-tooltip';
+
+interface CommentItemProps {
+  comment: CommentWithUser;
+}
+
+export function CommentItem({ comment }: CommentItemProps) {
+  const userCreatedAt = format(
+    new Date(comment.author.createdAt),
+    'MMMM  d  yyyy'
+  );
+  const createdAt = React.useMemo(() => {
+    return formatDates(comment.createdAt);
+  }, [comment.createdAt]);
+
+  const src = comment.author.externalImage ?? comment.author.profileImage ?? '';
+  const linkProfile = `/profile/${comment.author.externalId}`;
+  const firstName = comment.author.firstName;
+  const lastName = comment.author.lastName || '';
+  const verified = comment.author.verified;
+
+  return (
+    <div className='flex gap-2'>
+      <div>
+        <UserTooltip
+          imageUrl={src}
+          firstName={firstName}
+          lastName={lastName}
+          verified={verified}
+          userCreatedAt={userCreatedAt}
+          href={linkProfile}
+          size='md'>
+          <Link
+            href={linkProfile}
+            className='flex hover:brightness-105 transition-all duration-150 active:scale-95'>
+            <UserAvatar src={src} size='xs' />
+          </Link>
+        </UserTooltip>
+      </div>
+      <div className='flex flex-col gap-1'>
+        <div className='flex flex-col bg-accent rounded-xl p-2'>
+          <div className='flex'>
+            <Link href={linkProfile}>
+              <UserName
+                firstName={firstName}
+                lastName={lastName}
+                verified={verified}
+                className='text-xs md:text-sm font-medium text-foreground'
+                iconClassName='h-3 w-3'
+              />
+            </Link>
+          </div>
+          <p className='text-xs md:text-sm font-normal text-foreground'>
+            {comment.text}
+          </p>
+        </div>
+        <div className='flex ml-2 gap-3 text-xs font-medium text-muted-foreground'>
+          <p>Like</p>
+          <p>Reply</p>
+          <p>{createdAt}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,68 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import * as z from 'zod';
+import { toast } from 'react-hot-toast';
+import { isClerkAPIResponseError } from '@clerk/nextjs';
+import { formatDistanceToNowStrict } from 'date-fns';
 import { User } from '@prisma/client';
 import { FriendshipStatus } from '@/types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function catchClerkError(err: unknown) {
+  const unknownErr = 'Something went wrong, please try again later.';
+
+  if (err instanceof z.ZodError) {
+    const errors = err.issues.map((issue) => {
+      return issue.message;
+    });
+    return toast(errors.join('\n'));
+  } else if (isClerkAPIResponseError(err)) {
+    return toast.error(err.errors[0]?.longMessage ?? unknownErr);
+  } else {
+    return toast.error(unknownErr);
+  }
+}
+
+export function formatBytes(
+  bytes: number,
+  decimals = 0,
+  sizeType: 'accurate' | 'normal' = 'normal'
+) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const accurateSizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB'];
+  if (bytes === 0) return '0 Byte';
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+    sizeType === 'accurate' ? accurateSizes[i] ?? 'Bytest' : sizes[i] ?? 'Bytes'
+  }`;
+}
+
+export function formatDates(date: Date) {
+  const distance = formatDistanceToNowStrict(new Date(date));
+  const formattedDistance = distance
+    .replace('seconds', 's')
+    .replace('second', 's')
+    .replace('minutes', 'm')
+    .replace('minute', 'm')
+    .replace('hours', 'h')
+    .replace('hour', 'h')
+    .replace('days', 'd')
+    .replace('day', 'd')
+    .replace('months', 'mo')
+    .replace('month', 'mo')
+    .replace('years', 'y')
+    .replace('year', 'y');
+
+  return formattedDistance;
+}
+
+export function isArrayOfFile(files: unknown): files is File[] {
+  const isArray = Array.isArray(files);
+  if (!isArray) return false;
+  return files.every((file) => file instanceof File);
 }
 
 export function checkFriendship(
