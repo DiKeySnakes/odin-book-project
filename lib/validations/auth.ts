9cb6974230ba92f4c332349ffe7ccde8cd339f49
DiKeySnakes <dikeysnakes@gmail.com>
2024-01-13 12:45:11 +0200

    Modify package.json, package-lock.json, add lib/transition.ts, lib/validations/auth.ts, lib/validations/comment.ts, lib/validations/post.ts

---
+++ lib/validations/auth.ts
@@ -0,0 +1,47 @@
+import * as z from 'zod';
+
+export const loginUserSchema = z.object({
+  email: z.string().email({
+    message:
+      "The email address or mobile number you entered isn't connected to an account.",
+  }),
+  password: z
+    .string()
+    .min(8, {
+      message: 'Password must be at least 8 characters long',
+    })
+    .max(100)
+    .regex(/\d/, {
+      message: 'Password must contain at least one number',
+    }),
+});
+
+export const registerUserSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email({
+      message: 'Email is invalid',
+    }),
+  password: z
+    .string()
+    .min(8, {
+      message: 'Password must be at least 8 characters long',
+    })
+    .max(100)
+    .regex(/\d/, {
+      message: 'Password must contain at least one number',
+    }),
+});
+
+export const verifyEmailSchema = z.object({
+  code: z
+    .string()
+    .min(6, {
+      message: 'Verification code must be 6 characters long',
+    })
+    .max(6),
+});
